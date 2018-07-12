62b11b4d2e053d19257b4393b3e534d9b5fbc0c9
clement-daubrenet <clement.daubrenet@gmail.com>
2018-07-12 04:58:22 +0100

    Fix

--- celery_worker.py
+++ celery_worker.py
@@ -3,25 +3,31 @@ from tasks import create_app
 from tasks.tasks import create_contact, delete_contacts
 
 
-def create_celery(app):
-    celery = Celery(app.import_name,
-                    backend=app.config['CELERY_RESULT_BACKEND'],
-                    broker=app.config['BROKER_URL'])
-    celery.conf.update(app.config)
-    TaskBase = celery.Task
+def create_celery(flask_application):
+    """
+    Creating the Celery application instance.
+    :param obj flask_application: The Flask application instance.
+    :return:
+    """
+    celery_app = Celery(flask_application.import_name,
+                        backend=flask_application.config['CELERY_RESULT_BACKEND'],
+                        broker=flask_application.config['BROKER_URL'])
+    celery_app.conf.update(flask_application.config)
+    celery_task = celery_app.Task
 
-    class ContextTask(TaskBase):
+    class ContextTask(celery_task):
         abstract = True
 
         def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-    celery.Task = ContextTask
-    return celery
+            with flask_application.app_context():
+                return celery_task.__call__(self, *args, **kwargs)
+
+    celery_app.Task = ContextTask
+    return celery_app
 
 
-app = create_app()
-celery = create_celery(app)
+application = create_app()
+celery = create_celery(application)
 
 
 @celery.on_after_configure.connect
@@ -35,13 +41,13 @@ def setup_periodic_tasks(sender, **kwargs):
 
     # The task to create random contacts: every 15 seconds
     sender.add_periodic_task(15, create_contact,
-                             args=(app.config['CONTACT_API_URL'],),
+                             args=(application.config['CONTACT_API_URL'],),
                              name='Create every 15s')
 
     # The task to remove random contacts: every 60 seconds
     delete_from_seconds = 60
     sender.add_periodic_task(delete_from_seconds, delete_contacts,
-                             args=(app.config['CONTACT_API_URL'] + delete_from_seconds,),
+                             args=(application.config['CONTACT_API_URL'] + '/' + str(delete_from_seconds),),
                              name='Delete every 60s')
 
 
